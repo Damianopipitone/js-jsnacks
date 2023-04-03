69e69c9fca3c43f60fd1ff98b13d06c3bd5a86a7
Damianopipitone <damianopipitone33@gmail.com>
2023-04-03 15:45:55 +0200

    JSnack

--- js.snack-3/main.js
+++ js.snack-3/main.js
@@ -1,5 +1,12 @@
+/*Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Javascript - JS*/
+
 for (let i = 0; i < 10; i++) {
+    
     let chosenNumber = prompt("Inserisci un numero...");
+    
     let number = parseInt(chosenNumber);
+    
     console.log(number);
 };
