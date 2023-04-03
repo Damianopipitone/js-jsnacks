1fdff4937800b79d6fad331e44891dba94c63323
Damianopipitone <damianopipitone33@gmail.com>
2023-04-03 18:58:31 +0200

    JSname

--- js.snack-4/main.js
+++ js.snack-4/main.js
@@ -2,11 +2,11 @@
 Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
 no alla festa */ 
 
-const nameOnList = ("Luca", "Matilde", "Giovanni", "Elena", "Dario");
+const nameOnList = ["Luca", "Matilde", "Giovanni", "Elena", "Dario"];
 
 let yourName = prompt ("Inserisci il tuo nome...");
     
-if (yourName.includes = nameOnList) {
+if (nameOnList.includes(yourName)) {
     console.log("Il tuo nome è in lista, benvenuto!");
 } else {
     console.log("Mi spiace, ma il tuo nome non è in lista!");
